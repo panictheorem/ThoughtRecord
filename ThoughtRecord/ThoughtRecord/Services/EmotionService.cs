7c1c8c31b63bda4f01401700fe6c98f083ed387e
Andrew Pineau <andrewpineau@hotmail.com>
2016-05-26 14:09:27 -0400

    Fixed issue with situation display area being being too large on big screens.

--- ThoughtRecord/ThoughtRecord/Services/EmotionService.cs
+++ ThoughtRecord/ThoughtRecord/Services/EmotionService.cs
@@ -46,7 +46,7 @@ namespace ThoughtRecordApp.Services
                 "Obsessed",
                 "Emotionless",
                 "Overwhelmed",
-                "Paniced",
+                "Panicked",
                 "Alone",
             };
         }
