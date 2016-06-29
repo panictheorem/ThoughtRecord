3c6afa46f72f26bc897f204f15c48e6f52a09554
Andrew Pineau <andrewpineau@hotmail.com>
2016-06-29 12:52:55 -0400

    Fixed issue with menu colors by using a custom radio button class.

---
+++ ThoughtRecord/ThoughtRecord/Controls/MenuRadioButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace ThoughtRecordApp.Controls
+{
+    public class MenuRadioButton : RadioButton
+    {
+        protected override void OnPointerCaptureLost(PointerRoutedEventArgs e)
+        {
+            if (!(bool)IsChecked)
+            {
+                VisualStateManager.GoToState(this, "Normal", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Checked", false);
+            }
+        }
+        protected override void OnPointerEntered(PointerRoutedEventArgs e)
+        {
+            if (!(bool)IsChecked)
+            {
+                VisualStateManager.GoToState(this, "PointerOver", false);
+            }
+        }
+    }
+}
